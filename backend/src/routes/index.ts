f2d73b4b9ed0a9e883ba34e2794415de8e23061a
Brian <bng87@students.cs.ubc.ca>
2023-07-29 00:52:45 -0700

    added a mount for /items

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Application } from 'express';
 import users from './user.router';
 import events from './event.router';
 import shift from './shift.router';
+import items from './items.router';
 
 const mountRoutes = (app: Application) => {
     app.use('/user', users);
     app.use('/event', events);
     app.use('/shift', shift);
+    app.use('/items', items);
 }
 
 export default mountRoutes;
