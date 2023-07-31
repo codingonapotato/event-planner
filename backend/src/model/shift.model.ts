afed4bef330d6463a2a2a748a715483971be6ae9
Brian <bng87@students.cs.ubc.ca>
2023-07-30 21:05:39 -0700

    restructured post to grab neccessary fields from req.body

--- backend/src/model/shift.model.ts
+++ backend/src/model/shift.model.ts
@@ -14,33 +14,61 @@ export function get_using_volunteerID(id: number) {
     return db.query('SELECT * FROM shift WHERE volunteer_id = $1::integer', [id]);
 }
 
-// updates a shift's ID provided by the old [id: number] and new [new_id: string]
-export function updateSID(id: number, new_id: number) {
-    return db.query('UPDATE shift SET shift_id = $2::text WHERE shift_id = $1::integer', [id, new_id]);
+// // updates a shift's ID provided by the old [id: number] and new [new_id: string]
+// export function updateSID(id: number, new_id: number) {
+//     return db.query('UPDATE shift SET shift_id = $2::text WHERE shift_id = $1::integer', [id, new_id]);
+// }
+
+// // updates a shift's role provided by [id: number] and [role: string]
+// export function updateRole(id: number, role: string) {
+//     return db.query(`UPDATE shift SET role = $2::text WHERE shift_id = $1::integer`, [id, role]);
+// }
+
+// // updates a shift's start time provided by [id: number] and [startTime: string]
+// export function updateStartTime(id: number, startTime: string) {
+//     return db.query('UPDATE shift SET start_time = $2::timestamp WHERE shift_id = $1::integer', [id, startTime]);
+// }
+
+// // updates a shift's end time provided by [id: number] and [endTime: string]
+// export function updateEndTime(id: number, endTime: string) {
+//     return db.query('UPDATE shift SET end_time = $2::timestamp WHERE shift_id = $1::integer', [id, endTime]);
+// }
+
+// // updates a shift's station provided by [id: number] and [station: string]
+// export function updateStation(id: number, station: string) {
+//     return db.query('UPDATE shift SET station = $2::text WHERE shift_id = $1::integer', [id, station]);
+// }
+
+
+/**
+ * Updates all attributes with the exception of foreign keys in Shift
+ * @param id the shift_id of the shift we want to change
+ * @param new_id the new id to replace the old shift_id
+ * @param role 
+ * @param startTime 
+ * @param endTime 
+ * @param station 
+ * @returns promise to a query result
+ */
+export function updateShift(id: number, new_id: number, role: string, startTime: string, 
+    endTime: string, station: string) {
+        
+    return db.query(`UPDATE shift SET shift_id = $2::integer, role = $3::text, start_time = $4::timestamp, 
+        end_time = $5::timestamp, station = $6::text WHERE shift_id = $1::integer;`, 
+        [id, new_id, role, startTime, endTime, station]);
 }
 
-// updates a shift's role provided by [id: number] and [role: string]
-export function updateRole(id: number, role: string) {
-    return db.query(`UPDATE shift SET role = $2::text WHERE shift_id = $1::integer`, [id, role]);
-}
-
-// updates a shift's start time provided by [id: number] and [startTime: string]
-export function updateStartTime(id: number, startTime: string) {
-    return db.query('UPDATE shift SET start_time = $2::timestamp WHERE shift_id = $1::integer', [id, startTime]);
-}
-
-// updates a shift's end time provided by [id: number] and [endTime: string]
-export function updateEndTime(id: number, endTime: string) {
-    return db.query('UPDATE shift SET end_time = $2::timestamp WHERE shift_id = $1::integer', [id, endTime]);
-}
-
-// updates a shift's station provided by [id: number] and [station: string]
-export function updateStation(id: number, station: string) {
-    return db.query('UPDATE shift SET station = $2::text WHERE shift_id = $1::integer', [id, station]);
-}
-
-// adds a new shift using provided by the parameters: [id: number], [role: string], [startTime: string], 
-//    [endTime: string], [station: number], [volunteer_id: number], [event_id: number]
+/**
+ * adds a new shift detailed by the parameters
+ * @param id 
+ * @param role 
+ * @param startTime 
+ * @param endTime 
+ * @param station 
+ * @param volunteer_id 
+ * @param event_id 
+ * @returns promise to a QueryResult
+ */
 export function addShift(id: number, role: string, startTime: string, 
     endTime: string, station: string, volunteer_id: number, event_id: number) {
     return db.query(`INSERT INTO shift VALUES ($1::integer, $2::text, $3::timestamp, 
--- backend/src/routes/shift.router.ts
+++ backend/src/routes/shift.router.ts
@@ -52,10 +52,17 @@ shiftRouter.get('/volunteerID/:id', async (req, res) => {
    });
 }); 
 
-shiftRouter.post('/role/:id/:role', async (req, res) => {
+/**
+ * updates all attributes in shift given the initial shift_id in the URI
+ * Attributes to be changed are passed through req.body and they must 
+ * include: new_id, role, start_time, end_time, station
+ */
+shiftRouter.post('/:id', async (req, res) => {
    const id = parseInt(req.params.id);
-   const role = req.params.role.toString();
-   Shift.updateRole(id,role)
+
+   const {new_id, role, start_time, end_time, station} = req.body;
+
+   Shift.updateShift(id,new_id,role, start_time, end_time, station)
    .then((result) => {
       if (result.rowCount === 0) {
          res.status(404).send('shift not found') 
@@ -67,65 +74,82 @@ shiftRouter.post('/role/:id/:role', async (req, res) => {
    });
 }); 
 
-shiftRouter.post('/startTime/:id/:startTime', async (req, res) => {
-   const id = parseInt(req.params.id);
-   const startTime = req.params.startTime.toString();
-   Shift.updateStartTime(id,startTime)
-   .then((result) => {
-      if (result.rowCount === 0) {
-         res.status(404).send('shift not found') 
-      }
-      res.status(200).send(result.rows);
-   })
-   .catch((err) => {
-      res.status(500).send('Database query failed');
-   });
-}); 
 
-shiftRouter.post('/endTime/:id/:endTime', async (req, res) => {
-   const id = parseInt(req.params.id);
-   const endTime = req.params.endTime.toString();
-   Shift.updateStartTime(id,endTime)
-   .then((result) => {
-      if (result.rowCount === 0) {
-         res.status(404).send('shift not found') 
-      }
-      res.status(200).send(result.rows);
-   })
-   .catch((err) => {
-      res.status(500).send('Database query failed');
-   });
-});
 
-shiftRouter.post('/station/:id/:station', async (req, res) => {
-   const id = parseInt(req.params.id);
-   const station = req.params.station.toString();
-   Shift.updateStartTime(id,station)
-   .then((result) => {
-      if (result.rowCount === 0) {
-         res.status(404).send('shift not found') 
-      }
-      res.status(200).send(result.rows);
-   })
-   .catch((err) => {
-      res.status(500).send('Database query failed');
-   });
-});
+// shiftRouter.post('/role/:id/:role', async (req, res) => {
+//    const id = parseInt(req.params.id);
+//    const role = req.params.role.toString();
+//    Shift.updateRole(id,role)
+//    .then((result) => {
+//       if (result.rowCount === 0) {
+//          res.status(404).send('shift not found') 
+//       }
+//       res.status(200).send(result.rows);
+//    })
+//    .catch((err) => {
+//       res.status(500).send('Database query failed');
+//    });
+// }); 
 
-shiftRouter.post('/id/:id/:new_id', async (req, res) => {
-   const id = parseInt(req.params.id);
-   const new_id = parseInt(req.params.new_id);
-   Shift.updateSID(id,new_id)
-   .then((result) => {
-      if (result.rowCount === 0) {
-         res.status(404).send('shift not found') 
-      }
-      res.status(200).send(result.rows);
-   })
-   .catch((err) => {
-      res.status(500).send('Database query failed');
-   });
-});
+// shiftRouter.post('/startTime/:id/:startTime', async (req, res) => {
+//    const id = parseInt(req.params.id);
+//    const startTime = req.params.startTime.toString();
+//    Shift.updateStartTime(id,startTime)
+//    .then((result) => {
+//       if (result.rowCount === 0) {
+//          res.status(404).send('shift not found') 
+//       }
+//       res.status(200).send(result.rows);
+//    })
+//    .catch((err) => {
+//       res.status(500).send('Database query failed');
+//    });
+// }); 
+
+// shiftRouter.post('/endTime/:id/:endTime', async (req, res) => {
+//    const id = parseInt(req.params.id);
+//    const endTime = req.params.endTime.toString();
+//    Shift.updateStartTime(id,endTime)
+//    .then((result) => {
+//       if (result.rowCount === 0) {
+//          res.status(404).send('shift not found') 
+//       }
+//       res.status(200).send(result.rows);
+//    })
+//    .catch((err) => {
+//       res.status(500).send('Database query failed');
+//    });
+// });
+
+// shiftRouter.post('/station/:id/:station', async (req, res) => {
+//    const id = parseInt(req.params.id);
+//    const station = req.params.station.toString();
+//    Shift.updateStartTime(id,station)
+//    .then((result) => {
+//       if (result.rowCount === 0) {
+//          res.status(404).send('shift not found') 
+//       }
+//       res.status(200).send(result.rows);
+//    })
+//    .catch((err) => {
+//       res.status(500).send('Database query failed');
+//    });
+// });
+
+// shiftRouter.post('/id/:id/:new_id', async (req, res) => {
+//    const id = parseInt(req.params.id);
+//    const new_id = parseInt(req.params.new_id);
+//    Shift.updateSID(id,new_id)
+//    .then((result) => {
+//       if (result.rowCount === 0) {
+//          res.status(404).send('shift not found') 
+//       }
+//       res.status(200).send(result.rows);
+//    })
+//    .catch((err) => {
+//       res.status(500).send('Database query failed');
+//    });
+// });
 
 shiftRouter.put('/:id/:role/:startTime/:endTime/:station/:volunteer_id/:event_id', async (req, res) => {
    const id          = parseInt(req.params.id);
