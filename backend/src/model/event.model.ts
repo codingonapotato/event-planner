3efb55145877791ef9d4ce13ba185f7a031db5dc
Ricky Lau <rickylau2428@gmail.com>
2023-08-09 13:54:58 -0700

    Fix upcoming events query deleting duplicate tickets in the same tier

--- backend/src/model/event.model.ts
+++ backend/src/model/event.model.ts
@@ -188,7 +188,7 @@ export async function getUpcomingEvents(user_id: number) {
         city,
         province,
         COUNT(*) AS count
-        FROM event NATURAL JOIN city NATURAL JOIN province NATURAL JOIN ticket NATURAL JOIN tier
+        FROM event NATURAL JOIN city NATURAL JOIN province NATURAL JOIN ticket
         WHERE customer_id = $1 AND start_time >= (SELECT NOW())
         GROUP BY event_id, name, start_time, end_time, street_num, street, postal_code, city, province
         ORDER BY start_time`, [user_id]);
